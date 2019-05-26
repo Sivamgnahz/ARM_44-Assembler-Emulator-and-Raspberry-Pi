0cc36893cb3ea6df92a4892b27a680bfce616df3
Yifei Zhang <yz31218@edge06.doc.ic.ac.uk>
2019-05-26 22:58:22 +0100

    raw idea and head file  of assemble

---
+++ src/Assembler.h
@@ -0,0 +1,11 @@
+int main();
+
+int check_cond(int cond);
+
+int data_processing();
+int multiply();
+int data_transfer();
+int branch();
+int special();
+
+int get_opcode(char* mnemonic)
---
+++ src/pass.h
@@ -0,0 +1,1 @@
+int get_curr_instr_addr();
