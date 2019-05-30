c6e34c2c7123406aeab24df689d9972620be5ee9
Wenfei  Fang <wenfei.fang18@imperial.ac.uk>
2019-05-30 16:44:23 +0100

    Update assembler.h by Wenfei

--- src/assembler.h
+++ src/assembler.h
@@ -1,13 +1,16 @@
+#define u_int32_t unsigned int
+
 int main();
 
-char * data_pro1(char *instruction);
-char * data_pro2(char *instruction);
-char * data_pro3(char *instruction);
+u_int32_t data_pro1(char *instruction);
+u_int32_t data_pro2(char *instruction);
+u_int32_t data_pro3(char *instruction);
 
-char * regtoBits(char *reg);
-char * immtoBits(char *operand2);
-char * toBits(unsigned int x);
-char * hextoBits(char *hex);
+u_int32_t regtoBits(char *reg);
+u_int32_t immtoBits(char *operand2);
+u_int32_t concat(int cond, int i, int opcode, int s, int rn, int rd, int operand2);
+//char * toBits(unsigned int x);
+//char * hextoBits(char *hex);
 
 char * multiply();
 char * data_transfer();
